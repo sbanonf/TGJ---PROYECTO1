1ef04a31345e413918514f51c29a9ebebabaf7f9
DLamilla20 <lamilla.tenazoa@gmail.com>
2024-05-17 22:44:21 -0500

    PlayerBasico

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector2 mov;
+    public float speed = 5f;
+
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mov = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        mov.Normalize();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = mov * speed * Time.fixedDeltaTime;
+    }
+}
