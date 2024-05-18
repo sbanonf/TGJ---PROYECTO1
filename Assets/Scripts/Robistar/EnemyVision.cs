613f08e0624fbd0050d6a946d455c78d099fc3fd
sbanonf <sbanonf@gmail.com>
2024-05-17 23:33:45 -0500

    Robistar movement

--- Assets/Scripts/Robistar/EnemyVision.cs
+++ Assets/Scripts/Robistar/EnemyVision.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class EnemyVision : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public int waitingtime;
+    public float lerpDuration = 0;
+    private SpriteRenderer SpriteRenderer;
+    private bool flip = false;
+
+    public GameObject papa;
+
+    private void Start()
     {
-        
+        StartCoroutine(Timer());
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Me miraron feo");
+            //TimeManager.instance.time -=2;
+        }
+    }  
+
+    public IEnumerator Timer() {
+        while (true)
+        {
+            Debug.Log("Empeze timer");
+            yield return new WaitForSeconds(waitingtime);
+            Debug.Log("Termine timer");
+            StartCoroutine(LerpScale());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public IEnumerator LerpScale() {
+
+        float timeElapsed = 0;
+        Vector3 initialScale = papa.transform.localScale;
+        Vector3 targetScale = new Vector3(-initialScale.x, initialScale.y, initialScale.z);
+
+        while (timeElapsed < lerpDuration)
+        {
+            papa.transform.localScale = Vector3.Lerp(initialScale, targetScale, timeElapsed / lerpDuration);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+        papa.transform.localScale = targetScale;
+        flip = !flip;
+        papa.GetComponent<SpriteRenderer>().flipX = flip;
     }
 }
