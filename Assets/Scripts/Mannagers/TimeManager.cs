4a813ef656219ef3dd6fe6b5db48011c994a409e
sbanonf <sbanonf@gmail.com>
2024-05-18 01:08:51 -0500

    Time Manager

---
+++ Assets/Scripts/Mannagers/TimeManager.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public static TimeManager instance;
+    public TextMeshProUGUI tmpro;
+    public bool isGameFinished = false;
+    public bool CorreTiempo = false;
+    [SerializeField] public int TurnoIndex;
+    [SerializeField] public int NumeroTurnos;
+
+    public float tiempoxTurno = 40;
+    public float tiempoRuntime = 0;
+
+    private void Awake()
+    {
+        if (instance == null) { 
+            instance = this;
+        }
+    }
+    private void Start()
+    {
+        EmpezarTurno();
+    }
+    private void Update()
+    {
+        if (!isGameFinished)
+        {
+            if (CorreTiempo) {
+                if (tiempoRuntime > 0)
+                {
+                    tiempoRuntime -= Time.deltaTime;
+                    tmpro.text = Math.Round(tiempoRuntime).ToString();
+                }
+                else
+                {
+                    tiempoRuntime = 0;
+                    tmpro.text = Math.Round(tiempoRuntime).ToString();
+                    EmpezarTurno();
+                }
+            }            
+        }
+    }
+    public void EmpezarTurno() {
+        CorreTiempo = true;
+        TurnoIndex++;
+        tiempoRuntime = tiempoxTurno;
+        if (TurnoIndex < NumeroTurnos)
+        {
+
+        }
+        else {
+            Debug.Log("Fin del Juego");
+            SetGameOver();
+        }
+    }
+    public void SetGameOver() { 
+        isGameFinished = true; ;
+        //SceneManager.LoadScene("FinalBueno");
+    }
+}
