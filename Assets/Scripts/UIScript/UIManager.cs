2282487fdfab581ec6f7ac0881b101669211c2d2
Oscar Torres <oscar.torres.m@uni.pe>
2024-05-19 01:35:31 -0500

    tutorial panel

--- Assets/Scripts/UIScript/UIManager.cs
+++ Assets/Scripts/UIScript/UIManager.cs
@@ -6,6 +6,13 @@ public class UIManager : MonoBehaviour{
 
     [SerializeField] private GameObject BasePanel;
     [SerializeField] private GameObject CreditsPanel;
+    [SerializeField] private GameObject TutorialPanel;
+
+    private void Start(){
+        BasePanel.SetActive(true);
+        CreditsPanel.SetActive(false);
+        TutorialPanel.SetActive(false);
+    }
 
     public void ShowCreditsPanel(){
         BasePanel.SetActive(false);
@@ -15,5 +22,15 @@ public class UIManager : MonoBehaviour{
     public void DisableCreditsPanel(){
         BasePanel.SetActive(true);
         CreditsPanel.SetActive(false);
+    }
+
+    public void ShowTutorialPanel(){
+        BasePanel.SetActive(false);
+        TutorialPanel.SetActive(true);
+    }
+
+    public void DisableTutorialPanel(){
+        BasePanel.SetActive(true);
+        TutorialPanel.SetActive(false);
     }
 }
